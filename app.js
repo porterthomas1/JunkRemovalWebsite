c9b034a893f912e6a994a2051ed01bc10d27a3aa
porterthomas1 <103624496+porterthomas1@users.noreply.github.com>
2024-03-29 15:00:06 -0600

    header and footer adjustments

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+var scrollTimeout;
+var header = document.getElementById("main-header");
+var headerScrolled = document.getElementById("main-header.scrolled");
+var logoContainer = document.getElementById("logo-container");
+
+window.addEventListener("scroll", function () {
+  clearTimeout(scrollTimeout);
+
+  // Add a small delay before applying the transition
+  scrollTimeout = setTimeout(function () {
+    // Calculate the scroll position threshold
+    var scrollThreshold = headerScrolled;
+
+    // Check if the scroll position exceeds the threshold
+    if (window.scrollY > scrollThreshold) {
+      header.classList.add("scrolled");
+      logoContainer.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+      logoContainer.classList.remove("scrolled");
+    }
+  }, 30); // Adjust the delay as needed
+});
